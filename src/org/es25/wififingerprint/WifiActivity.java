8f16e078e2c537471b67a0c8756a47fffc4ccc6c
rauz <c.rauecker@vcnc.org>
2015-05-04 19:48:57 +0200

    all new files added final alpha 0.2b

---
+++ src/org/es25/wififingerprint/WifiActivity.java
@@ -0,0 +1,60 @@
+package org.es25.wififingerprint;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class WifiActivity extends Activity {
+	
+	private MyBroadcastAPList APListUpdateReceiver;
+	String Test;
+	
+	private static final String TAG = "WifiActivity";
+
+	
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);		
+		setContentView(R.layout.activity_wifi);
+		final Intent WifiIntent = new Intent(this, WifiFingerPrintDaemon.class);
+		
+		
+		 final Button startBT = (Button) findViewById(R.id.startBT);
+		 
+		  startBT.setOnClickListener(new View.OnClickListener() {
+		        public void onClick(View v) {
+		       	 startService(WifiIntent);
+		        }
+		    });		
+		  
+		IntentFilter intentFilter = new IntentFilter(WifiFingerPrintDaemon.NOTIFICATION);
+		intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+	    registerReceiver(APListUpdateReceiver, intentFilter);
+	}
+	
+	
+	public class MyBroadcastAPList extends BroadcastReceiver {
+
+		  @Override
+		  public void onReceive(Context context, Intent intent) {
+		   Test = intent.getStringExtra(WifiFingerPrintDaemon.APRESULT);
+		  
+		  }
+		 }
+	
+  
+
+	
+}
