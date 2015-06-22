7ee79bd3ec3541b5195d657aa5b86be8bf8741df
Armin Leghissa <vargsgrimm@gmail.com>
2015-06-22 15:24:17 +0200

    added documentation.

--- src/org/es25/wififingerprint/Util.java
+++ src/org/es25/wififingerprint/Util.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2015 Armin Leghissa, Christian Rauecker
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
 
 package org.es25.wififingerprint;
@@ -31,8 +48,8 @@ public class Util {
 	/**
 	 * Calculates a percentage value for the signal quality from a given rssi value.
 	 *
-	 * @param dBm The dB/RSSI value to convert.
-	 * @return Quality level.
+	 * @param dBm The dBm/RSSI value to convert.
+	 * @return Quality level as a percentage value.
 	 */
 	public static int rssi2quality(int dBm) {
 		if (dBm >= -50) {
--- src/org/es25/wififingerprint/struct1/Location.java
+++ src/org/es25/wififingerprint/struct1/Location.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2015 Armin Leghissa, Christian Rauecker
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
 
 package org.es25.wififingerprint.struct1;
@@ -7,6 +24,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+/**
+ * Location object.
+ * It consists of a location name which is typically an user-defined name or the ESSID
+ * as well as a list of {@link Station}s, the actual access points belonging to ths location.
+ *
+ * @author Armin Leghissa
+ */
 public class Location {
 
 	String name;
@@ -14,6 +38,11 @@ public class Location {
 	Map<String, Integer> ap_map;
 
 
+	/**
+	 * Creates a new location specifying it's name.
+	 *
+	 * @param name User-defined name or ESSID.
+	 */
 	public Location(String name) {
 		this.name = name;
 		stations = new ArrayList<Station>();
@@ -21,28 +50,55 @@ public class Location {
 	}
 
 
+	/**
+	 * Adds a new acces point specifying mac address and rssi value.
+	 *
+	 * @param mac MAC address.
+	 * @param rssi RSSI/quality value.
+	 */
 	public void addStation(String mac, int rssi) {
 		stations.add(new Station(mac, rssi));
 		ap_map.put(mac, rssi);
 	}
 
 
+	/**
+	 * Adds a new access point specifying a {@link Station} object.
+	 *
+	 * @param station A station to add.
+	 */
 	public void addStation(Station station) {
 		stations.add(station);
 		ap_map.put(station.mac, station.rssi);
 	}
 
 
+	/**
+	 * Gets the name of this location.
+	 *
+	 * @return this station's name.
+	 */
 	public String getName() {
 		return this.name;
 	}
 
 
+	/**
+	 * Gets a list of all accespoints hold by this location.
+	 *
+	 * @return this station's access points.
+	 */
 	public ArrayList<Station> getStations() {
 		return this.stations;
 	}
 
 
+	/**
+	 * Gets the RSSI value for the specifyed mac address.
+	 *
+	 * @param mac A MAC address which appropriate RSSI value to retrieve.
+	 * @return the appropriate RSSI value or NULL if it does not exist.
+	 */
 	public Integer getRssiValue(String mac) {
 		for (Station d : stations) {
 			if (mac.equals(d.mac)) {
@@ -53,6 +109,12 @@ public class Location {
 	}
 
 
+	/**
+	 * Gets the RSSI value for the specifyed mac address.
+	 *
+	 * @param mac A MAC address which appropriate RSSI value to retrieve.
+	 * @return the appropriate RSSI value or NULL if it does not exist.
+	 */
 	public Integer getRssiFor(String mac) {
 		return ap_map.get(mac);
 	}
